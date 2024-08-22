36f435eb26866da8d6256d655bebe5772aabddb5
Dicka Ismaji <dickaismaji@gmail.com>
2024-08-22 15:46:36 +0700

    fix: responsive modal

--- components/Modal.vue
+++ components/Modal.vue
@@ -2,8 +2,8 @@
   <div class="bg-black/40 inset-0 fixed transition-all"
     :class="{ 'opacity-100 visible': open, 'opacity-0 invisible': !open }">
     <div class="relative inset-0">
-      <div class="absolute left-1/2 -translate-x-1/2 top-[200px]">
-        <div class="bg-white rounded-xl p-6 w-[500px] max-w-[500px] space-y-6 mx-4">
+      <div class="absolute left-1/2 -translate-x-1/2 top-[200px] w-full p-6">
+        <div class="bg-white rounded-xl p-6 mx-auto w-full sm:max-w-[500px] space-y-6">
           <header class="flex justify-between">
             <h2 class="text-3xl">{{ title }}</h2>
             <div>
